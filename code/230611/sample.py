4c8a7b3421afc09eebbec5880eaceaa8ae04b3ba
bsmoon-86 <ekflafg@naver.com>
2023-06-17 23:05:23 +0900

    update

---
+++ code/230611/sample.py
@@ -0,0 +1,64 @@
+import pandas as pd 
+import os 
+
+def list_df(path, end_string):
+    # file의 list를 변수로 생성
+    file_list = os.listdir(path)
+    # 확장자가 end_string과 같은 경우의 리스트만 추출
+    file_list2 = []
+    # 반복문을 이용하여 end_string과 같은 확장자만 파일만 file_list2에 추가
+    # for file in file_list:
+    #     if (file.split(".")[-1] == end_string):
+    #         file_list2.append(file)
+    
+    for file in file_list:
+        if (file.endswith(end_string)):
+            file_list2.append(file)
+
+    # file_list2가 완성이 되면 
+    # file_list2를 이용하여 데이터프레임을 결합
+
+    # 비어있는 데이터프레임을 생성 
+    result = pd.DataFrame()
+
+    # for문을 이용하여 file_list2 파일들을 비어있는 데이터프레임에 결합
+    for file in file_list2:
+        print(path + file)
+        if(end_string == 'csv'):
+            data = pd.read_csv(path + file)
+        elif (end_string == 'json'):
+            data = pd.read_json(path + file)
+        elif (end_string == 'xlsx') | (end_string == 'xls'):
+            data = pd.read_excel(path + file)
+        
+        # result와 data 라는 2개의 데이터프레임을 유니언 결합
+        result = pd.concat([result, data], axis=0, ignore_index=True)
+    
+    return result
+
+# 폴더에 있는 파일의 리스트를 하나의 Dataframe으로 만드는 함수 호출
+bank_df = list_df('../sample/', 'xls')
+tax_df = list_df('../sample/', 'xlsx')
+
+# 데이터프레임을 그룹화 하여 준다. 
+df1 = tax_df.groupby(['상호', '대표자명']).sum().reset_index()
+df2 = bank_df.groupby('보낸분/받는분').sum()[['입금액(원)']].reset_index()
+
+# df2의 컬럼의 이름을 [대표자명, 입금액]로 변경
+df2.columns = ['대표자명', '입금액']
+
+# total_df는 세금계산서와 은행거래를 join 결합
+total_df = pd.merge(left = df1, right=df2, on='대표자명', how='left')
+
+# 차액 파생변수를 생성 
+# 합계금액 - 입금액
+total_df['차액'] = total_df['합계금액'] - total_df['입금액']
+
+# 미수여부
+# 입금완료 -> 차액이 0이하인 경우 
+# 미수 -> 차액이 0초과인 경우
+total_df.loc[total_df['차액'] > 0, '미수여부'] = '미수'
+total_df.loc[total_df['차액'] <= 0, '미수여부'] = '입금완료'
+
+# 데이터프레임을 엑셀로 저장
+total_df.to_excel('result.xlsx', index=False)
